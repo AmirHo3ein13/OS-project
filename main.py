65dae030e7a7afb1e94e8118aa6700bfbe17693a
AmirHossein Bahrami <a.bahrami9675@gmail.com>
2018-01-15 01:38:21 +0330

    add time and join for threads

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 import os
 from threading import Thread
-# TODO: somewhere we should use time :)
 import time
 
 
-def n3_order_func(n):
+def n3_order_func(n, number):
+    print('Thread number = ', number, '\tprocess id = ', os.getpid())
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 pass
 
 
-class MyThread(Thread):
-    def __init__(self, n):
-        Thread.__init__(self)
-        self.n = n
-
-    def run(self):
-        n3_order_func(self.n)
-
-
 # n = number of executions
 # f = number of processes
 # t = number of threads
-n, f, t = [int(x) for x in input().split()]
 
+# n, f, t = [int(x) for x in input().split()]
+n, f, t = 1000, 2, 95
+
+tm = time.time()
+
+# TODO: fix termination problem
+p_threads = dict()
 for i in range(f):
-    # TODO: what should we do here?!?!
     parent = os.fork()
     if not parent:
+        p_threads[os.getpid()] = []
         for j in range(t):
-            # th = MyThread(n)
-            # th.start()
-            t = Thread(target=n3_order_func, args=(n,))
-            t.start()
+            print('make thread ', j)
+            p_threads[os.getpid()].append(Thread(target=n3_order_func, args=(n, j)))
+        counter = 1
+        for thr in p_threads[os.getpid()]:
+            print('start thread ', counter)
+            counter += 1
+            thr.start()
+        counter = 1
+        for thr in p_threads[os.getpid()]:
+            print('join thread ', counter)
+            counter += 1
+            thr.join()
+    else:
+        print('Process id = ', os.getpid())
+
+
+time.sleep(100)
+print(time.time() - tm)
 
