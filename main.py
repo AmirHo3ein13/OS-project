5e07f6f4bd603685df41609b4a98c68595545885
AmirHossein Bahrami <a.bahrami9675@gmail.com>
2018-01-18 23:31:11 +0330

    comment unnecessary lines

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# n = 129 and 645
 import os
 from threading import Thread
 import time
 
 
 def n3_order_func(n, number):
-    print('Thread number = ', number, '\tprocess id = ', os.getpid())
+    # print('Thread number = ', number, '\tprocess id = ', os.getpid())
     for i in range(n):
         for j in range(n):
             for k in range(n):
@@ -15,34 +16,37 @@ def n3_order_func(n, number):
 # f = number of processes
 # t = number of threads
 
-# n, f, t = [int(x) for x in input().split()]
-n, f, t = 1000, 2, 95
+n, f, t = [int(x) for x in input().split()]
+# n, f, t = 100, 2, 95
 
 tm = time.time()
 
-# TODO: fix termination problem
-p_threads = dict()
+# p_threads = dict()
 for i in range(f):
     parent = os.fork()
     if not parent:
-        p_threads[os.getpid()] = []
+        threads = []
         for j in range(t):
-            print('make thread ', j)
-            p_threads[os.getpid()].append(Thread(target=n3_order_func, args=(n, j)))
-        counter = 1
-        for thr in p_threads[os.getpid()]:
-            print('start thread ', counter)
-            counter += 1
+            # print('make thread ', j)
+            threads.append(Thread(target=n3_order_func, args=(n, j)))
+        # counter = 1
+        for thr in threads:
+            # print('start thread ', counter)
+            # counter += 1
             thr.start()
-        counter = 1
-        for thr in p_threads[os.getpid()]:
-            print('join thread ', counter)
-            counter += 1
+        # counter = 1
+        for thr in threads:
+            # print('join thread ', counter)
+            # counter += 1
             thr.join()
-    else:
-        print('Process id = ', os.getpid())
+
+        exit()
+    # else:
+    #     print('Process id = ', os.getpid())
 
 
-time.sleep(100)
-print(time.time() - tm)
+if parent:
+    for i in range(f):
+        os.waitpid(-1, 0)
+    print('\033[94m', time.time() - tm, '\033[0m')
 
