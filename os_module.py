553f921036e43d82400a92ca0d34f5ca65628f83
AmirHossein Bahrami <a.bahrami9675@gmail.com>
2018-01-20 18:41:13 +0330

    add comment to module

--- os_module.py
+++ os_module.py
@@ -13,22 +13,29 @@ def n3_order_func(n):
 def run(n, f, t):
     tm = time.time()
 
+    # make child process
     for i in range(f):
         parent = os.fork()
         if not parent:
             threads = []
+
+            # init threads
             for j in range(t):
                 threads.append(Thread(target=n3_order_func, args=(n,)))
 
+            # start threads
             for thr in threads:
                 thr.start()
 
+            # wait till threads finishes
             for thr in threads:
                 thr.join()
 
+            # end the process
             exit()
 
     if parent:
+        # wait for all child processes to finish
         for i in range(f):
             os.waitpid(-1, 0)
         return [n, f, t, time.time() - tm]
